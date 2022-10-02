562189dd1510ad549e113b1a57aba1f1295c5f4e
Sidhant <sidhant26sood@gmail.com>
2022-10-03 00:30:39 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import "./Sidebar/Navbar"
 import Navbar from './Sidebar/Navbar';
+import Login from './Login';
+import { BrowserRouter as Router, Routes, Route, Redirect} from "react-router-dom";
+
 
 export default function App() {
   return (
+    <Router>
     <div className="App">
-      <Navbar />
+      <Routes>
+      <Route exact path="/">
+        <Navbar />
+      </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Routes>
+      
     </div>
+    </Router>
   );
 }
--- src/Sidebar/Navbar.js
+++ src/Sidebar/Navbar.js
@@ -5,33 +5,33 @@ import './custom.scss'
 import { FaList, FaRegHeart } from "react-icons/fa";
 import {
   FiHome,
-  FiLogOut,
+  FiLogIn,
   FiArrowLeftCircle,
   FiArrowRightCircle
 } from "react-icons/fi";
 import { RiPencilLine } from "react-icons/ri";
 import { BiCog } from "react-icons/bi";
+import "../Login"
+import { Router, Link } from "react-router-dom";
+
 
 export default function Navbar()
 {  
+  
     const menuIconClick = () => {
-        //condition checking to change state from true to false and vice versa
         menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
       };
       const [menuCollapse, setMenuCollapse] = useState(false);
     return (
-        <>
+      
         <div id="header">
-          {/* collapsed props to change menu size using menucollapse state */}
           <div className="sideBar">
           <ProSidebar collapsed={menuCollapse}>
             <SidebarHeader>
               <div className="logotext">
-                {/* small and big change using menucollapse state */}
                 <p>{menuCollapse ? "Logo" : "Big Logo"}</p>
               </div>
               <div className="closemenu" onClick={menuIconClick}>
-                {/* changing menu collapse icon on click */}
                 {menuCollapse ? <FiArrowRightCircle /> : <FiArrowLeftCircle />}
               </div>
             </SidebarHeader>
@@ -48,12 +48,15 @@ export default function Navbar()
             </SidebarContent>
             <SidebarFooter>
               <Menu iconShape="square">
-                <MenuItem icon={<FiLogOut />}>Logout</MenuItem>
+                <MenuItem icon={<FiLogIn />}>
+                  Login
+                  {/* <Link to ="Login" /> */}
+                 </MenuItem>
               </Menu>
             </SidebarFooter>
           </ProSidebar>
         </div>
         </div>
-      </>
+      
       );
 }
