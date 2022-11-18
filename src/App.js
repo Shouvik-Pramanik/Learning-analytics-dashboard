5d440b1e3b91831301ffc45fc197f7be5165a8d0
Sidhant <sidhant26sood@gmail.com>
2022-11-18 17:19:04 +0530

    Added Flask server

--- src/App.js
+++ src/App.js
@@ -3,16 +3,8 @@ import "./Sidebar/Navbar"
 import Navbar from './Sidebar/Navbar';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LoginForm from './Login/LoginForm';
-import { useEffect } from 'react';
 
 export default function App() {
-  useEffect(()=>{
-      fetch("/members").then(res=>res.json()).then(
-        e=>{
-          console.log(e)
-        }
-      )
-    },[])
   return (
     
     <>
